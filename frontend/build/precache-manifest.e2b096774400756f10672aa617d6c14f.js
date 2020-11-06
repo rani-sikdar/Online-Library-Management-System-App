36104d246d9de1663f5f85361b845638b21e36a0
Rani Sikdar <73461140+rani-sikdar@users.noreply.github.com>
2020-11-06 22:58:12 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.e2b096774400756f10672aa617d6c14f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11072b354ca5b9c55232113bfdab82ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964e465bdb8bb35dca84",
+    "url": "/static/css/main.1ff7b789.chunk.css"
+  },
+  {
+    "revision": "5b2f0d982790fa8fb38a",
+    "url": "/static/js/2.f84a988e.chunk.js"
+  },
+  {
+    "revision": "b4f9955a46235ab5d9673fafd3d3f6c5",
+    "url": "/static/js/2.f84a988e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "964e465bdb8bb35dca84",
+    "url": "/static/js/main.9ae569c2.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  }
+]);
